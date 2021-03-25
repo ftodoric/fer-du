1c26f8d21cf6e83420f9becf345d23b6d5f06d21
ftodoric <fptodoric@gmail.com>
2021-03-25 22:56:50 +0100

    Create lab01/

--- test-workspace/test.py
+++ test-workspace/test.py
@@ -1,3 +1,13 @@
-import py_learn
+import torch
 
-print("test.py")
+
+def f(x, a, b):
+    return a*x + b
+
+
+a = torch.tensor(5., requires_grad=True)
+b = torch.tensor(8., requires_grad=True)
+x = torch.tensor(2.)
+y = f(x, a, b)
+s = a ** 2
+print(s)
