1b0d44df2ec4cc7d27c4bac4a536e445d61ba1c9
ftodoric <fptodoric@gmail.com>
2021-04-13 20:27:39 +0200

    Restructure files

--- lab00/binlogreg.py
+++ lab00/binlogreg.py
@@ -1,16 +1,20 @@
 import numpy as np
+import data
 
 
 # stable softmax
-def stable_softmax(x):
-    exp_x_shifted = np.exp(x - np.max(x))
-    probs = exp_x_shifted / np.sum(exp_x_shifted)
-    return probs
+def softmax(s):
+    return np.exp(s) / (1 + np.exp(s))
 
 
-def sigma_P(y, s):
+def sigma_P(y, scores):
     # binary classification - classes: 0 and 1
-    return np.exp(s) ** y / (1 + np.exp(s))
+    probs = []
+    for i in range(len(y)):
+        prob = np.exp(scores[i, 0])**(1 - y[i])/(1 + np.exp(scores[i, 0]))
+        probs.append([prob])
+    probs = np.array(probs)
+    return probs
 
 
 def binlogreg_train(X, Y_):
@@ -23,11 +27,11 @@ def binlogreg_train(X, Y_):
         w, b: log reg parameters
     """
 
-    param_niter = 10
-    param_delta = 0.1
+    param_niter = 10000
+    param_delta = 1e-2
 
     w = np.random.randn(X.shape[1], 1)
-    b = np.zeros((1, X.shape[0]))
+    b = 0
 
     # grad descent (param_niter number of iterations)
     for i in range(param_niter):
@@ -35,36 +39,51 @@ def binlogreg_train(X, Y_):
         scores = np.dot(X, w) + b
 
         # class probabilities c_1 - Nx1
-        probs = stable_softmax(scores)
+        probs = softmax(scores)
 
         # loss - scalar
         loss = np.sum(-np.log(sigma_P(Y_, scores))) * 1/len(X)
 
         # dijagnostički ispis
         if i % 10 == 0:
-            pass
-        print("iteration {}: loss {}".format(i, loss))
+            print("iteration {}: loss {}".format(i, loss))
 
         # loss derivations by scores - Nx1
-        iverson = 1 * (probs > 0.5)
+        iverson = 1 * (probs < 0.5)
         dL_ds = probs - iverson
 
         # parameters gradients
         grad_w = 1 / len(X) * np.dot(np.transpose(dL_ds), X)
-        print(grad_w)
-        grad_b = 1 / len(X) * np.transpose(dL_ds)
-        print(grad_b)
+        grad_b = 1 / len(X) * np.sum(dL_ds)
 
         # update parameters
-        w += -param_delta * np.transpose(grad_w)
-        b += -param_delta * np.transpose(grad_b)
+        w -= param_delta * np.transpose(grad_w)
+        b -= param_delta * np.transpose(grad_b)
+
+    return w, b
+
+
+def binlogreg_classify(X, w, b):
+    return softmax(np.dot(X, w) + b)
 
 
 # MAIN
-X = np.array([[1, 3],
-              [3, 2],
-              [1, 2]])
-Y_ = np.array([[0],
-               [1],
-               [0]])
-binlogreg_train(X, Y_)
+if __name__ == '__main__':
+    np.random.seed(100)
+
+    # get the training dataset
+    X, Y_ = data.sample_gauss_2d(2, 100)
+
+    # train the model
+    w, b = binlogreg_train(X, Y_)
+
+    # evaluate the model on the training dataset
+    probs = binlogreg_classify(X, w, b)
+    Y = np.round(probs[:,0]).astype(int)
+
+    # report performance
+    accuracy, recall, precision = data.eval_perf_binary(Y, Y_)
+    AP = data.eval_AP(Y_[probs.argsort()])
+    print(accuracy)
+    print(recall, precision, AP)
+
