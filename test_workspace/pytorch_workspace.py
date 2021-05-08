a8468c72aad1f27b97ef2b69b3a252de2c8fb438
ftodoric <fptodoric@gmail.com>
2021-05-08 02:14:47 +0200

    Minor changes

--- test_workspace/pytorch_workspace.py
+++ test_workspace/pytorch_workspace.py
@@ -1,3 +1,29 @@
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
+
+# definiranje operacije
+
+
+def f(x, a, b):
+    return a * x + b
+
+
+# definiranje varijabli i izgradnja dinamičnog
+# računskog grafa s unaprijednim prolazom
+a = torch.tensor(5., requires_grad=True)
+b = torch.tensor(8., requires_grad=True)
+x = torch.tensor(2.)
+y = f(x, a, b)
+s = a ** 2
+
+# unatražni prolaz koji računa gradijent
+# po svim tenzorima zadanim s requires_grad=True
+y.backward()
+s.backward()               # gradijent se akumulira
+assert x.grad is None      # pytorch ne računa gradijente po x
+assert a.grad == x + 2 * a  # dy/da + ds/da
+assert b.grad == 1         # dy/db + ds/db
+
+# ispis rezultata
+print(f"y={y}, g_a={a.grad}, g_b={b.grad}")
